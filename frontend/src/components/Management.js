3a6fb8f77e3f9d50a97cd1cf34cadad61a68bef3
Verapan <smverappan@gmail.com>
2025-02-09 16:48:34 +0530

    backend api fix

--- frontend/src/components/Management.js
+++ frontend/src/components/Management.js
@@ -36,7 +36,7 @@ const Management = () => {
   // Fetch faculty data and organize it by course
   const fetchData = async () => {
     try {
-      const response = await axios.get('http://faculty-selection.onrender.com/faculty');
+      const response = await axios.get('http://faculty-selection.onrender.com/');
       setFacultyData(response.data);
 
       const courseMap = {};

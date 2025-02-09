970a87f1ebf9338b694bc7e076b00ddaf20c274c
Verapan <smverappan@gmail.com>
2025-02-09 16:45:57 +0530

    backend api point fixed

--- frontend/src/components/Management.js
+++ frontend/src/components/Management.js
@@ -36,7 +36,7 @@ const Management = () => {
   // Fetch faculty data and organize it by course
   const fetchData = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/faculty');
+      const response = await axios.get('http://faculty-selection.onrender.com/faculty');
       setFacultyData(response.data);
 
       const courseMap = {};

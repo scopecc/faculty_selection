d4c95db5ccd1b8b6097dcca9ab9ad7868719919d
scopecc <scopecc.wat@vit.ac.in>
2026-04-19 11:36:22 +0530

    Allow Common users to select Common Group 3 in Theory Courses

--- frontend/src/components/CourseSelection.js
+++ frontend/src/components/CourseSelection.js
@@ -608,14 +608,15 @@ useEffect(() => {
 {Object.keys(theoryCoursesByDomain)
   .filter(domain => {
     if (userDomain === "Common") {
-      // Common user: only Common Group 1 and Common Group 2
-      return domain === "Common Group 1" || domain === "Common Group 2";
+      // Common user: only Common Group 1 and Common Group 2 and Common Group 3
+      return domain === "Common Group 1" || domain === "Common Group 2"|| domain === "Common Group 3";
     } else {
-      // Other users: their domain + Common Group 1 + Common Group 2
+      // Other users: their domain + Common Group 1 + Common Group 2 + Common Group 3
       return (
         domain === userDomain ||
         domain === "Common Group 1" ||
-        domain === "Common Group 2"
+        domain === "Common Group 2" ||
+        domain === "Common Group 3"
       );
     }
   })

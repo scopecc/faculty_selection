73294a7d9fa265058600ff5955b891bf035033ca
scopecc <scopecc.wat@vit.ac.in>
2026-09-10 09:56:33 +0530

    Simplify course selection instructions

--- frontend/src/components/CourseSelection.js
+++ frontend/src/components/CourseSelection.js
@@ -375,7 +375,7 @@ useEffect(() => {
         <strong>Please read the instructions given below before proceeding with the registration process:</strong>
       </p>
       <ol style={{ color: "red", textAlign: "left", fontWeight: "bold", marginLeft: "20px", fontSize: "16px" }}>
-        <li>You must select exactly {maxCourses} courses in any combination from both sections(Theory+Lab and Theory)</li>
+        <li>You must select exactly {maxCourses} courses.</li>
         <li>The selected courses will be displayed in the order in which you select them.</li>
       </ol>
 
